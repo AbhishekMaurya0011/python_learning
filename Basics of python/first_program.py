64b5be9274150b7d890a148c8ecbe9d6ba58f4a5
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-09-18 13:34:04 +0530

    Add files via upload

---
+++ Basics of python/first_program.py
@@ -0,0 +1,7 @@
+# This is a first program of the python print the Hello World and Enter your name by input funtion
+
+a=input("Enter Your First Name:")
+b=input("Enter Your Last name:")
+print("Hello World")
+print("The Given Name is:")
+print(a+" "+b)
