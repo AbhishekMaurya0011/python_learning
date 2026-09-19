81c70356e9e324a5fcd2d694ac6c7d153a51f538
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-09-19 09:16:38 +0530

    Add files via upload

---
+++ Basics of python/check_even_or_odd.py
@@ -0,0 +1,6 @@
+#Accept an integer and check whether it is an even number or odd.
+num=int(input("Enter Number N:"))
+if(num%2==0):
+    print("The Given Number is Even")
+else:
+    print("The Given Number is odd")
---
+++ Basics of python/compare2Numbers.py
@@ -0,0 +1,10 @@
+# accept two number and print the greatest between them.
+a= int(input("Enter Number A:"))
+b= int(input("Enter Number B:"))
+
+if(a>b):
+    print("A is Greater than B")
+
+else:
+    print("B is Greater than A")
+
