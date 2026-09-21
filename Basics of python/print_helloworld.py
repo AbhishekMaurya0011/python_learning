125e916d9ecb4fb7983953b8e45ac6eb2c80e1f8
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-09-21 22:30:45 +0530

    Add files via upload

---
+++ Basics of python/print_helloworld.py
@@ -0,0 +1,4 @@
+#WAP to print Hello World to n times and number is given by the user.
+n=int(input("Enter Number to Want How Many Times to Print Hello World:"))
+for i in range(n):
+    print("Hello World")
